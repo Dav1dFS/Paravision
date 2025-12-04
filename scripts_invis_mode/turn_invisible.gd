7d51554a52fd00cb99c454bce79475ad9506d038
MC Andarilho <macrt2000@gmail.com>
2025-12-04 01:22:28 +0000

    invisibility sort of works now

--- scripts_invis_mode/turn_invisible.gd
+++ scripts_invis_mode/turn_invisible.gd
@@ -2,10 +2,16 @@ extends Node
 
 var invis_toggle:bool
 
+#the objects material
+@export var invis_mat:StandardMaterial3D
+
+#the object itself
+@export var obj:CSGBox3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
+
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -13,10 +19,15 @@ func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("Invis_Toggle"):
 		invis_toggle = !invis_toggle
 		
+		#makes object invisible and deactivates colisions
 		if invis_toggle == true:
 			print("invisible bitch")
+			invis_mat.albedo_color = "ff00ff00"
+			obj.use_collision = false
 			
-			
+		#makes object visible again and activates colisions
 		if invis_toggle == false:
 			print("visible bitch")
+			invis_mat.albedo_color = "ff00ff"
+			obj.use_collision = true
 	pass
