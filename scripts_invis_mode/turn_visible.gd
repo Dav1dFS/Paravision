1f97aabedae8babdd85fdbaa7377b3f6015e80fe
MC Andarilho <macrt2000@gmail.com>
2025-12-06 18:29:14 +0000

    invisibility changer done

--- scripts_invis_mode/turn_visible.gd
+++ scripts_invis_mode/turn_visible.gd
@@ -2,10 +2,16 @@ extends Node
 
 var invis_toggle:bool
 
-@export var static_obj:StaticBody3D
+@export var area_obj:Area3D
 
 #the objects material
 @export var invis_mat:StandardMaterial3D
+
+@export var respawn_point:CSGBox3D
+
+@export var player:CharacterBody3D
+
+var respawn_coords:Vector3
 
 var og_color:Color
 
@@ -18,6 +24,8 @@ func _ready() -> void:
 	
 	#save original color
 	og_color = invis_mat.albedo_color
+	
+	respawn_coords = respawn_point.transform.origin
 	
 	pass # Replace with function body.
 
@@ -39,6 +47,10 @@ func _process(delta: float) -> void:
 			print("invis_toggle OFF")
 			invis_mat.albedo_color = "ff00ff"
 			#colision_shape.set_deferred("disabled", false)
-			
-	
 	pass
+
+
+func _on_body_entered(body: Node3D) -> void:
+	if body.is_in_group("Player"):
+			print("you died!")
+			player.transform.origin = respawn_coords
